ab33d50a17d969f7344c10683d42aee8b18a396b
David Angulo <davidarmandoangulogil@gmail.com>
2026-02-07 17:13:49 -0700

    No sé como decir que no se usar Git
    Added new constructor and additional operator overloads.

--- Matriz.hpp
+++ Matriz.hpp
@@ -1,17 +1,25 @@
 #pragma once
+#include <iostream>
 
 class Matriz {
+    friend std::ostream & operator<<(std::ostream & out, Matriz m);
+    friend Matriz operator*(float escalar, Matriz m);
+
 public:
   /* ---- CONSTRUCTORES ---- */
-  Matriz(int n = 2, int m = 2); // Constructor por default
+  explicit Matriz(int n = 2, int m = 2, float valor = 0); // Constructor por default
   ~Matriz();                    // Destructor
-  Matriz(const Matriz &m);
+  Matriz(const Matriz &m);      // Constructor de Copias
   /*-------------------------*/
 
   /* ---- MÉTODOS DE LA CLASE ---- */
   void Imprimir() const;
-  Matriz operator+(Matriz m);
+  Matriz operator+(Matriz m) const;
+  Matriz operator-(Matriz m) const;
+  Matriz operator*(Matriz m) const;
+  Matriz operator*(float escalar) const;
   Matriz &operator=(const Matriz &m);
+  Matriz Transpuesta() const;
 
 private:
   // Una matriz se representa con dimensiónes n*m
