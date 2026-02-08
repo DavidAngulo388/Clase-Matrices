7a2c813bffa1a75561d7112a27a8b3e774501483
David Angulo <davidarmandoangulogil@gmail.com>
2026-02-07 17:13:09 -0700

    Operator*,<<, +, - y transpuestas añadidos

--- Matriz.cpp
+++ Matriz.cpp
@@ -11,7 +11,7 @@ using std::endl;
 // Constructor por default
 
 // Constructor
-Matriz::Matriz(int n, int m) {
+Matriz::Matriz(int n, int m, float valor) {
   if (n <= 0 || m <= 0) {
     throw "Dimensi\162n inv\160lida";
   }
@@ -36,7 +36,7 @@ Matriz::Matriz(int n, int m) {
   // Llenamos la matriz de puros ceros (menos batalla que dar la identidad).
   for (int i = 0; i < renglones; ++i) {
     for (int j = 0; j < columnas; ++j) {
-      entradas[i][j] = 0;
+      entradas[i][j] = valor;
     }
   }
 }
@@ -53,16 +53,6 @@ Matriz::~Matriz() {
     delete[] entradas[i];
   }
   delete[] entradas;
-}
-
-/* -------------------------------------------------- */
-void Matriz::Imprimir() const {
-  for (int i = 0; i < renglones; ++i) {
-    for (int j = 0; j < columnas; ++j) {
-      cout << entradas[i][j] << " ";
-    }
-    cout << endl;
-  }
 }
 
 Matriz &Matriz::operator=(const Matriz &m) {
@@ -99,17 +89,98 @@ Matriz &Matriz::operator=(const Matriz &m) {
   return *this;
 }
 
-Matriz Matriz::operator+(Matriz m) {
+/* -------------------------------------------------- */
+Matriz Matriz::operator+(Matriz m) const{
   if (renglones != m.renglones || columnas != m.columnas) {
     throw "Error: Operaci\162n fallida. Dimensiones distintas.";
   }
 
-  Matriz resultante;
-  for (int i = 0; i < renglones; ++i) {
-    for (int j = 0; j < columnas; ++j) {
-      continue;
+  Matriz matriz_suma(renglones, columnas);
+  for (int i = 0; i < matriz_suma.renglones; ++i) {
+    for (int j = 0; j < matriz_suma.columnas; ++j) {
+      matriz_suma.entradas[i][j] = entradas[i][j] + m.entradas[i][j];
     }
   }
 
-  return resultante;
+  return matriz_suma;
 }
+
+/* -------------------------------------------------- */
+Matriz Matriz::operator-(Matriz m) const{
+  if (renglones != m.renglones || columnas != m.columnas) {
+    throw "Error: Operaci\162n fallida. Dimensiones distintas.";
+  }
+
+  Matriz matriz_resta(renglones, columnas);
+  for (int i = 0; i < matriz_resta.renglones; ++i) {
+    for (int j = 0; j < matriz_resta.columnas; ++j) {
+      matriz_resta.entradas[i][j] = entradas[i][j] - m.entradas[i][j];
+    }
+  }
+
+  return matriz_resta;
+}
+
+/* -------------------------------------------------- */
+Matriz Matriz::operator*(Matriz m) const{
+  if (columnas != m.renglones) {
+    throw "Error: Operaci\162n fallida. Dimensiones distintas.";
+  }
+
+  Matriz matriz_mult(renglones, m.columnas);
+  for(int i = 0; i < renglones; ++i){
+    for(int j = 0; j < m.columnas; ++j){
+      for(int k = 0; k < columnas; ++k){
+          matriz_mult.entradas[i][j] += entradas[i][k] * m.entradas[k][j];
+      }
+    }
+  }
+  return matriz_mult;
+}
+
+/* -------------------------------------------------- */
+Matriz Matriz::operator*(float escalar) const{
+    Matriz matriz_escalar(renglones, columnas);
+    for(int i = 0; i < matriz_escalar.renglones; ++i){
+        for(int j = 0; j < matriz_escalar.columnas; ++j){
+            matriz_escalar.entradas[i][j] = escalar * entradas[i][j];
+        }
+    }
+    return matriz_escalar;
+}
+
+/* -------------------------------------------------- */
+Matriz Matriz::Transpuesta() const{
+    Matriz matriz_trans(columnas, renglones);
+    for(int i = 0; i < renglones; ++i){
+        for(int j = 0; j < columnas; ++j){
+            matriz_trans.entradas[j][i] = entradas[i][j];
+        }
+    }
+    return matriz_trans;
+}
+
+/* ----FUNCIONES AMIGAS--- */
+Matriz operator*(float escalar, Matriz m){
+    Matriz matriz_escalar(m.renglones, m.columnas);
+    for(int i = 0; i < matriz_escalar.renglones; ++i){
+        for(int j = 0; j < matriz_escalar.columnas; ++j){
+            matriz_escalar.entradas[i][j] = escalar * m.entradas[i][j];
+        }
+    }
+    return matriz_escalar;
+}
+
+/* ----FUNCIONES OPERADORAS AMIGAS--- */
+std::ostream & operator<<(std::ostream & out, Matriz m){
+    out << endl;
+    for(int i = 0; i < m.renglones; ++i){
+        for(int j = 0; j < m.columnas; ++j){
+            out << m.entradas[i][j] << " ";
+        }
+        out << endl;
+    }
+    out << "\b";
+    return out;
+}
+
