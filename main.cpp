0194467a7a06b8120de054133c1df3c1a9ef73b2
David Angulo <davidarmandoangulogil@gmail.com>
2026-02-07 17:14:27 -0700

    Pruebas de los operadores añadidos

--- main.cpp
+++ main.cpp
@@ -7,11 +7,17 @@ int main() {
   // TODO: Menusito bonito perronson aca.
 
   try {
-    Matriz m(4, 10);
-    Matriz m_2(4, 10);
+    Matriz m(2, 3, 34);
+    Matriz m_2(2, 3, 33);
 
     Matriz suma = m + m_2;
-
+    cout << "SUMA: " << m << "+" << m_2 << "=" << suma << endl;
+    cout << "RESTA: " << m << "-" << m_2 << "=" << m-m_2 << endl;
+    Matriz m_3(3,2,5);
+    cout << "MULTIPLICACION DE MATRICES: " << m << "*" << m_3 << "=" << m*m_3 << endl;
+    float c = 3.58f;
+    cout << "MULTIPLICACION ESCALAR (c*m): " << c << endl << "*" << m << "=" << c*m << endl;
+    cout << "MULTIPLICACION ESCALAR (m_2*c): " << m_2 << "*" << c << endl << "=" << m_2*c << endl;
   } catch (const char *mensaje) {
     cerr << mensaje << endl;
   }
